741b89892c05f4c25dd4c75226565fe6df5e4cb2
py1u.dev <py1u.dev@gmail.com>
2026-04-13 20:48:00 -0700

    feat: add single airport detailed fetch function

--- test.py
+++ test.py
@@ -3,7 +3,10 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import time
+from fuzzysearch import find_near_matches
+
 from tabulate import tabulate as tbl
+
 
 # function to collect BTS airport data for all common US airports
 # note: part of web scraping for data ingestion
@@ -11,6 +14,7 @@ def get_bts_airports(ss: requests.Session, url: str, agent: dict, max_tries: int
 
     for attempt in range(max_tries):
         try:
+            print("fetching all airports in United States..")
             res = ss.get(url, headers=agent, timeout=5)
 
             if res.status_code == 200:
@@ -32,6 +36,7 @@ def get_bts_airports(ss: requests.Session, url: str, agent: dict, max_tries: int
                             "url": extracted_url
                         })
 
+                print("Successfully fetched!")
                 return airports
 
             else:
@@ -46,7 +51,56 @@ def get_bts_airports(ss: requests.Session, url: str, agent: dict, max_tries: int
 
 # same input data except for the url which must be a single airport
 def get_bts_airport_details(ss: requests.Session, url: str, agent: dict, max_tries: int = 5, backoff: int = 1.5):
-    pass
+    for attempt in range(max_tries):
+        try:
+            res = ss.get(url, headers=agent, timeout=5)
+
+            if res.status_code == 200:
+                soup = BeautifulSoup(res.text, "html.parser")
+                airport_data = {}
+
+                # extract all relevant airport metadata
+                scripts = soup.find_all("script")
+
+                tbl_names =  ("Summary Data (U.S. Flights Only)", # keyword
+                              "Top 10 Destination Airports (U.S. Only, Passengers (000))", # found in third div but for now do not use
+                              "{airport_code_here} On-Time Performance Summary (Major U.S. Carriers Only)", # for now do not use
+                              )
+
+                # parse html data tables
+                th = soup.find("th", string=lambda s: s and tbl_names[0] in s)
+
+                if not th:
+                    return None
+
+                container = th.find_parent("tr")
+
+                tables = container.find_all("table")
+
+                parsed_tables = []
+
+                for table in tables:
+                    rows = table.find_all("tr")
+
+                    parsed = []
+
+                    for row in rows:
+                        cols = row.find_all(["td", "th"])
+                        parsed.append([c.get_text(strip=True) for c in cols])
+
+                    parsed_tables.append(parsed)
+
+                return parsed_tables
+
+            else:
+                print(f"Attempt {attempt + 1} failed with status code: {res.status_code}")
+
+        except requests.exceptions.RequestException as e:
+            print(f"Attempt {attempt + 1} error: {e}")
+
+        time.sleep(backoff ** attempt)
+
+    raise Exception(f"Failed to fetch data for airport after {max_tries} attempts")
 
 
 # test
@@ -56,8 +110,13 @@ headers = { "User-Agent": "Mozilla/5.0", "Referer": "https://www.transtats.bts.g
 
 airports = get_bts_airports(ss=session, url=bts_url, agent=headers)
 
-for airport in airports[:10]:
-    print(airport)
+def fetch_single_airport_simple(keyword: str):
+    for airport in airports:
+        if keyword in airport["name"]: # testing for ATL
+            print(airport)
+            break
+
+fetch_single_airport_simple("Hartsfield")
 
 # df_test = pd.read_excel("data/table_01_44_032626.xlsx", sheet_name="1-44")
 # print(df_test["Table 1-44: Passengers Boarded at the Top 50 U.S. Airports"].head(10))
