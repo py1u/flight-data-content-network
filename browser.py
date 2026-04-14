1921a13833bb3961aac5e8d77d4af342bc7eeea0
py1u.dev <py1u.dev@gmail.com>
2026-04-14 14:37:59 -0700

    update: addterminal statements for workflow

--- browser.py
+++ browser.py
@@ -2,20 +2,30 @@ from playwright.sync_api import sync_playwright
 
 """
 - on BTS transtats website, some sites are blocked via Akamai CDN making bot and webscraping difficult with requests and bs4 alone.
-- using playwright to support browser automation.  
+- using playwright to support browser automation.
+
+TODO:  
 """
 
 # create method to simulate playwright browser access
 def fetch_html(url: str):
 
+
+
+    # browser is run on chromium
     with sync_playwright() as p:
+
+        print("starting playwright browser..")
+
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
 
         page.goto(url, wait_until="networkidle")
 
-        html = page.content()
+        print("page(HTML) content received!")
+        html = page.content() # returned html content
 
+        print("closing playwright browser.")
         browser.close()
 
     return html
